dd5021f1917ae62c1480f61f3d12d3746f90bdf0
Gabriel Dahia <gdahia@gmail.com>
2018-08-31 17:22:11 -0300

    Add classifier abstract class

---
+++ classifier.py
@@ -0,0 +1,14 @@
+class Classifier:
+  def __init__(self,
+               units_ls,
+               n_classes,
+               W_prior=None,
+               b_prior=None,
+               logits_prior=None):
+    pass
+
+  def train(self, images, labels):
+    pass
+
+  def infer(self, image):
+    pass
