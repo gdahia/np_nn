828c97f0f95bc56886e915cd49f75184ce37a223
Gabriel Dahia <gdahia@gmail.com>
2018-09-01 15:12:46 -0300

    Make Classifier abstract

--- classifier.py
+++ classifier.py
@@ -1,23 +1,22 @@
-class Classifier:
-  def __init__(self,
-               units_ls,
-               n_classes,
-               W_prior=None,
-               b_prior=None,
-               logits_prior=None):
+from abc import ABC, abstractmethod
+
+
+class Classifier(ABC):
+  @abstractmethod
+  def infer(self, x):
     pass
 
+  @abstractmethod
   def _forward(self, images, labels):
     pass
 
+  @abstractmethod
   def _backward(self):
     pass
 
+  @abstractmethod
   def _update(self):
     pass
 
   def train(self, images, labels, learning_rate):
-    pass
-
-  def infer(self, image):
     pass
