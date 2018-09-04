Fix softmax not specifying axis

classifier/linear_softmax.py
@@ -60,7 +60,7 @@ class LinearSoftmax(Classifier):
     x = np.reshape(x, (-1, self.Ws[0].shape[0]))
     for W, b in zip(self.Ws, self.bs):
       x = np.matmul(x, W) + b
-    return nn.softmax(x)
+    return nn.softmax(x, axis=1)
 
   def _forward(self, inputs, labels):
     # flatten 'inputs'
@@ -72,10 +72,12 @@ class LinearSoftmax(Classifier):
       xs = np.matmul(xs, W) + b
       activations.append(xs)
 
+    # compute predictions
+    preds = nn.softmax(xs, axis=1)
+    activations.append(preds)
+
     # compute loss
-    activations.append(nn.softmax(xs))
-    loss = np.mean(
-        nn.softmax_cross_entropy_with_logits(labels=labels, logits=xs))
+    loss = np.mean(nn.cross_entropy(probs=labels, preds=preds, axis=1))
 
     return activations, loss
 

nn.py
@@ -1,10 +1,10 @@
 import numpy as np
 
 
-def softmax(x):
-  x_ = x - np.max(x)
+def softmax(x, axis=None):
+  x_ = x - np.max(x, axis=axis, keepdims=True)
   exp_x = np.exp(x_)
-  return exp_x / np.sum(exp_x)
+  return exp_x / np.sum(exp_x, axis=axis, keepdims=True)
 
 
 def sigmoid(x):
@@ -23,6 +23,15 @@ def softmax_cross_entropy_with_logits(labels, logits):
   # TODO: compare numerically both forms of softmax using tf implementation
   # cross_entropy = -np.sum(labels * (logits - np.log(np.sum(np.exp(logits)))), axis=1)
   cross_entropy = -np.sum(labels * np.log(softmax(logits)), axis=1)
+
+
+def cross_entropy(probs, preds, axis=None):
+  # make arguments be np arrays
+  probs = np.array(probs)
+  preds = np.array(preds)
+
+  # compute cross entropy
+  cross_entropy = -np.sum(probs * np.log(preds), axis=axis, keepdims=True)
 
   return cross_entropy
 